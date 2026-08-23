aad871c791dea73f1fe24796c4dd20de9ca3c74e
brandnakash-code <brandnakash@gmail.com>
2026-08-23 13:27:54 -0700

    Update bot

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,36 @@
 use serenity::prelude::*;
 use std::env;
+use std::sync::Mutex;
 
-struct Handler;
+struct Handler {
+    prefix: Mutex<char>,
+}
 
 #[serenity::async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: serenity::model::channel::Message) {
-        if msg.content.eq_ignore_ascii_case("!ping") {
-            let _ = msg.channel_id.say(&ctx.http, "Pong!").await;
+        if msg.author.bot {
+            return;
         }
-    }
 
-    async fn ready(&self, _: Context, ready: serenity::model::gateway::Ready) {
-        println!("{} is connected!", ready.user.name);
+        let prefix = self.prefix.lock().unwrap();
+
+        if msg.content.starts_with(*prefix) {
+            let _ = msg.channel_id.say(&ctx.http, "No commands yet!");
+        }
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let token = env::var("DISCORD_TOKEN")
-        .expect("DISCORD_TOKEN environment variable is missing");
+    let token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN environment variable is missing");
 
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
+        .event_handler(Handler {
+            prefix: Mutex::new('!'),
+        })
         .await
         .expect("Error creating client");
 
